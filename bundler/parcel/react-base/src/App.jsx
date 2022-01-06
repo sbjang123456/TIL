ba978a8782eab514fb64ea58a80d039e58777542
JangSubin <sbjang123456@gmail.com>
2022-01-07 00:45:55 +0900

    Setting parcel react base - dev

---
+++ bundler/parcel/react-base/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StrongText from './components/StrongText';
+
+const App = ()=>{
+    return (
+        <div>
+            Parcel Test <StrongText label="Confirm"/>
+        </div>
+    );
+}
+
+export default App;
