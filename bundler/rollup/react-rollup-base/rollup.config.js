62c33d323844761e671bdc72d001fdeade160265
JangSubin <sbjang123456@gmail.com>
2022-01-19 17:31:54 +0900

    [#18] 각 번들러 바벨 적용 (#32)
    * [#18] Modify package.json + add .parcelrc
    
    * [#18] Add babel rollup, add dev watch option and livereload
    
    * [#18] apply babel webpack

--- bundler/rollup/react-rollup-base/rollup.config.js
+++ bundler/rollup/react-rollup-base/rollup.config.js
@@ -4,6 +4,8 @@ import commonjs from '@rollup/plugin-commonjs';
 import html from '@rollup/plugin-html';
 import serve from 'rollup-plugin-serve';
 import replace from '@rollup/plugin-replace';
+import babel from '@rollup/plugin-babel'
+import livereload from 'rollup-plugin-livereload'
 
 export default {
     input: 'src/index.tsx',
@@ -13,12 +15,25 @@ export default {
     },
     plugins: [
         replace({
+            preventAssignment: true,
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
         }),
         resolve(),
+        babel({
+            presets: ["@babel/preset-react"],
+            // babelHelpers: 'runtime',
+        }),
         commonjs(),
         typescript(),
         html(),
-        process.env.NODE_ENV === 'development' && serve('build')
+        process.env.NODE_ENV === 'development' && serve('build'),
+        // process.env.NODE_ENV === 'development' && serve({
+        //     open: true,
+        //     verbose: true,
+        //     contentBase: ["", "public"],
+        //     host: "localhost",
+        //     port: 10001,
+        // }),
+        livereload({ watch: "build" }),
     ]
 };
