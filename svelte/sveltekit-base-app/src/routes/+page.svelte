170dbbd4f0d82ce5bdbc387ae810efd14d658663
JangSubin <sbjang123456@gmail.com>
2023-02-05 15:06:20 +0900

    feat(svelte): create sveltekit 템플릿, 간단 작성 (#134)

---
+++ svelte/sveltekit-base-app/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  let name = 'sb';
+</script> 
+
+<h1>Welcome to SvelteKit {name}</h1>
+<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+
+<style>
+  h1 {
+    font-size: 30px;
+  }
+</style>
---
+++ svelte/sveltekit-base-app/src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+
+<script>
+  export let data
+</script>
+
+<h1> about page {data.title}</h1>
+
+<form action="/api1" method="post">
+  <input type="text" name="meail" />
+  <input type="text" name="password" />
+  <button>로그인</button>
+</form>
