76a66dc7a6d370a842a90ad414f8c01aff0968a9
sbjang <sbjang123456@gmail.com>
2024-01-01 18:24:28 +0900

    svelte 예제 소스 정리

--- svelte/sveltekit-base-app/src/routes/api1/+server.ts
+++
@@ -1,7 +0,0 @@
-export async function GET({ fetch, request }) {
-  return new Response(JSON.stringify({ name: 'jang' }));
-}
-
-export async function POST({ fetch, request }) {
-  return new Response(JSON.stringify({ name: 'post jang' }));
-}
